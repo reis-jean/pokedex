64069c9ad5df26c4cd820a448b920b13c8c95931
jean reis <jvictor4919@gmail.com>
2020-05-07 10:14:49 -0300

    uptade

--- app.py
+++ app.py
@@ -2,19 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-
-def index():
-    pokemons = [
+pokemons = [
         ['bulbasaur','https://i.imgur.com/ntcCJr4.png'],
         ['charmander','https://i.imgur.com/5HmVqlW.png'],
         ['squirtle', 'https://i.imgur.com/h2aCkl3.png'],
         ['pikachu', 'https://i.imgur.com/soCJ6J5.png']
     ]
+
+@app.route('/')
+def index():
     return render_template(
         'index.html',
         titulo='pokedex',
         pokemons = pokemons
+    )
+
+@app.route('/poke/<int:id>')
+def pokemon(id):
+    poke = pokemons[id]
+    return render_template('pokemon.html',
+        pokemon = poke 
     )
 
 if __name__ == '__main__':
